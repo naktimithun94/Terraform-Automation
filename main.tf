be56066e07b51ddcad50163aed817a6f86f39ab7
naktimithun94 <naktimithun94@gmail.com>
2026-04-16 14:09:54 +0530

    Add vpc_id to jenkins-sg-2022 security group

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main" {
 resource "aws_security_group" "jenkins-sg-2022" {
   name        = var.security_group
   description = "security group for Ec2 instance"
-
+    vpc_id = aws_vpc.main.id
   ingress {
     from_port   = 8080
     to_port     = 8080
