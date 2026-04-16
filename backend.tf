4bb447590f57246c68feca981495452f52d2cb23
naktimithun94 <naktimithun94@gmail.com>
2026-04-16 13:43:58 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-123"
+    bucket = "mydev-project-terraform-sample-batch-2994"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
